39d77d7474e86dd245f34eb4daa8015753d02a01
kavyaHB <kavyahbgowda1998@gmail.com>
2021-04-17 22:28:51 +0530

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def sample1(request):
+    return render(request,"sample1.html")
+
+def sample2(request,data):
+    return render(request,"sample2.html",context={'data':data})
+
+def sample3(request):
+    return render(request,"sample3.html",context={'name':"kavya"})
